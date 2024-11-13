b53244b4db63c954d1b425af01d14c0b2a595b4d
Thomas Jones1 <thomas.jones1@students.snow.edu>
2024-11-13 13:27:29 -0700

    added sample data

---
+++ sample_data.sql
@@ -0,0 +1,26 @@
+insert into plane_type(plane_name) values
+	('Boeing 737-200'),
+	('Boeing 737-220'),
+	('Boeing 747-400'),
+	('Boeing 757-020'),
+	('Airbus A300-03');
+	
+insert into seat_type(seat_type) values
+	('coach'),
+	('business class'),
+	('first class');
+	
+insert into overbooking_rate(rate) values
+	(2.25);
+	
+insert into airport(code, address) values
+	('JFK', 'Queens, NY 11430, US'),
+	('LAX', '1 World Way, Los Angeles, CA 90045, US'),
+	('SEA', '17801 International Blvd, Pmb 68727, Seattle WA 98158, US'),
+	('ORD', '10000 W Balmoral Ave, Chicago IL 60666, US'),
+	('MDW', '5700 S Cicero Ave, Chicago IL 60638, US'),
+	('SFO', 'San Francisco, CA 94128, US'),
+	('SLC', 'W Terminal Dr, Salt Lake City, UT 84122, US'),
+	('DEN', '8500 Pena Blvd. Denver, CO 80249, US'),
+	('AGS', '1501 Aviation Way, Augusta, GA 30906, US'),
+	('ANC', '5000 W International Airport Rd, Anchorage, AK 99502, US');
