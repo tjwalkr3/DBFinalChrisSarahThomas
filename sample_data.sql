6fd074853167dc3001e9ac02c9ac08a2d4475245
Christopher Young <c.t.ythegamer@gmail.com>
2024-11-14 15:17:36 -0700

    Flight history

--- sample_data.sql
+++ sample_data.sql
@@ -1,19 +1,19 @@
-insert into plane_type(plane_name) values
+insert into airline_booking.plane_type(plane_name) values
 	('Boeing 737-200'),
 	('Boeing 737-220'),
 	('Boeing 747-400'),
 	('Boeing 757-020'),
 	('Airbus A300-03');
 	
-insert into seat_type(seat_type) values
+insert into airline_booking.seat_type(seat_type) values
 	('coach'),
 	('business class'),
 	('first class');
 	
-insert into overbooking_rate(rate) values
+insert into airline_booking.overbooking_rate(rate) values
 	(2.25);
 	
-insert into airport(code, address) values
+insert into airline_booking.airport(code, address) values
 	('JFK', 'Queens, NY 11430, US'),
 	('LAX', '1 World Way, Los Angeles, CA 90045, US'),
 	('SEA', '17801 International Blvd, Pmb 68727, Seattle WA 98158, US'),
@@ -25,7 +25,7 @@ insert into airport(code, address) values
 	('AGS', '1501 Aviation Way, Augusta, GA 30906, US'),
 	('ANC', '5000 W International Airport Rd, Anchorage, AK 99502, US');
 
-insert into plane_type_seat_type (plane_type_id, seat_type_id, quantity) values
+insert into airline_booking.plane_type_seat_type (plane_type_id, seat_type_id, quantity) values
 	(1, 1, 120),
 	(2, 1, 165),
 	(2, 2, 14),
@@ -39,7 +39,7 @@ insert into plane_type_seat_type (plane_type_id, seat_type_id, quantity) values
 	(5, 2, 12),
 	(5, 3, 6);
 
-insert into plane (plane_type_id) values
+insert into airline_booking.plane (plane_type_id) values
 	(1), -- 5 Boeing 737-200 planes
 	(1),
 	(1),
@@ -69,14 +69,23 @@ insert into scheduled_flight (
 		arrival_airport_id, 
 		overbooking_id  
 	)  values
-	-- Flights scheduled on 08/16/2024  -- Plane 1, Boeing 737-200 (only coach seats available)  
+	-- Flights scheduled on 08/16/2024  
+	-- Plane 1, Boeing 737-200 (only coach seats available)  
 	('2024-08-16 04:30:00', '2024-08-16 06:35:00', 1, 7, 2, 1), -- 2hr 5min from SLC to LAX  
 	('2024-08-16 06:50:00', '2024-08-16 13:00:00', 1, 2, 1, 1), -- 5hr 25min from LAX to JFK  
 	('2024-08-16 13:15:00', '2024-08-16 19:30:00', 1, 1, 3, 1), -- 6hr 15min from JFJ ti SEA  
 	-- Plane 2, Boeing 737-200 (only coach seats available)  
 	('2024-08-16 00:05:00', '2024-08-16 07:15:00', 2, 10, 9, 1), -- 7hr 10min from ANC to AGS  
-	('2024-08-16 04:30:00', '2024-08-16 04:30:00', 2, 9, 5, 1), -- 1hr 45min from AGS to MDW  
-	('2024-08-16 04:30:00', '2024-08-16 04:30:00', 2, 5, 6, 1); -- 4hr from MDW to SFO
+	('2024-08-16 04:30:00', '2024-08-16 06:30:00', 2, 9, 5, 1), -- 1hr 45min from AGS to MDW  
+	('2024-08-16 06:45:00', '2024-08-16 08:45:00', 2, 5, 6, 1); -- 4hr from MDW to SFO
+	
+insert into flight_history
+(scheduled_flight_id, plane_id, actual_departure_time, actual_arrival_time, delay)
+values (1, 1, '2024-08-16 04:40:00', '2024-08-16 06:45:00', '10 minutes'),
+		(2, 1, '2024-08-16 07:20:00', '2024-08-16 13:20:00', '20 minutes'),
+		(4, 2,'2024-08-16 00:15:00', '2024-08-16 07:30:00', '15 minutes'),
+		(5, 2, '2024-08-16 05:30:00', '2024-08-16 07:30:00', '1 hour'),
+		(6, 2, default, default, default);
 	
 insert into airline_booking.passenger
 (passenger_name, passport_id, phone, email, address)
@@ -88,7 +97,7 @@ values ('Thomas Jones', '123456789', '801-420-6666', 'thomas@gmail.com', '123 W 
 		('Cody Howell', '444555666', '801-420-7777', 'cody@gmail.com',  '121 W 323 S, Chicago, IL'),
 		('Nathan Howell', '777888999', '801-420-8888', 'nathan@gmail.com', '333 W 444 S, Layton, UT');
 	
-insert into reservation 
+insert into airline_booking.reservation 
 (passenger_id, scheduled_flight_id, ticket_cost)
 values (1, 1, 200.00),
 		(2, 1, 200.00),
@@ -96,7 +105,7 @@ values (1, 1, 200.00),
 		(6, 5, 200.00),
 		(5, 5, 150.00);
 	
-insert into seat
+insert into airline_booking.seat
 (reservation_id, seat_type_id, printed_boarding_pass_at, seat_number, passenger_id)
 values (1, 1, '2024-08-16 02:30:00',1, 1),
 		(1, 1, '2024-08-16 02:31:00',2, 4)
@@ -106,7 +115,7 @@ values (1, 1, '2024-08-16 02:30:00',1, 1),
 		(4, 1, '2024-08-16 02:00:00',30, 6),
 		(5, 1, default, DEFAULT, default);
 	
-INSERT INTO payment
+INSERT INTO airline_booking.payment
 (reservation_id, amount)
 VALUES (1, 400.00),
 		(1, 3.99),
@@ -117,7 +126,7 @@ VALUES (1, 400.00),
 		(4, 200.00),
 		(5, 150.00);
 	
-insert into product (concession_name, price) values
+insert into airline_booking.product (concession_name, price) values
 	('Pillow', 8.16),
 	('Blanket', 6.12),
 	('Headphones', 20.99),
@@ -125,13 +134,13 @@ insert into product (concession_name, price) values
 	('Fountain drink', 2.89),
 	('Chewing gum', 1.99);
 		
-INSERT INTO concession_purchase
+INSERT INTO airline_booking.concession_purchase
 (payment_id, seat_id)
 VALUES (2, 1),
 		(3, 2),
 		(5, 3);
 
-INSERT INTO concession_purchase_product
+INSERT INTO airline_booking.concession_purchase_product
 (product_id, concession_purchase_id, quantity)
 VALUES (4, 1, 1),
 		(5, 2, 2),
