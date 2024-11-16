89211425403b6ec5698447e1414e18b702562c4a
Christopher Young <c.t.ythegamer@gmail.com>
2024-11-16 15:42:56 -0700

    DbProduct class

---
+++ AirportFlightScheduler/AirportFlightScheduler/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportFlightScheduler
+{
+    public class DbProduct
+    {
+        public string ConcessionName { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+    }
+}
