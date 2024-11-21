faa667c020e14badd62d5b564dd3ce268f43b8ad
= <=>
2024-11-20 21:34:48 -0700

    Added plane_total_flight_time view

---
+++ Phase3/views.sql
@@ -0,0 +1,10 @@
+create view plane_total_flight_time as
+	select
+		p.id plane_id,
+		sum(age(fh.actual_arrival_time, fh.actual_departure_time))
+	from
+	flight_history fh
+	inner join plane p on (p.id = fh.plane_id)
+	where (fh.actual_departure_time is not null and fh.actual_arrival_time is not null)
+	group by p.id
+	order by p.id;
