705e27907d78239feb4a9f6e23c61ac2c84d86a2
= <=>
2024-11-20 21:39:31 -0700

    Renamed column in view

--- Phase3/views.sql
+++ Phase3/views.sql
@@ -1,7 +1,7 @@
 create view plane_total_flight_time as
 	select
 		p.id plane_id,
-		sum(age(fh.actual_arrival_time, fh.actual_departure_time))
+		sum(age(fh.actual_arrival_time, fh.actual_departure_time)) total_hours_flight_time
 	from
 	flight_history fh
 	inner join plane p on (p.id = fh.plane_id)
